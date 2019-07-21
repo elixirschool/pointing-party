799a8d6fc666f7b8ee9674c19db7af1dc2b8cc1e
Michael Stalker <mtstalker@gmail.com>
2019-07-21 17:00:41 -0400

    Restyle user list

--- assets/js/users.js
+++ assets/js/users.js
@@ -15,20 +15,20 @@ const updateUsers = presence => {
 const userData = (userId, { metas: [{ points }, ..._rest]}) => ({ userId, points })
 
 const showPoints = usersElem => ({userId, points}) => {
-  const userElem = document.querySelector(`.${userId} .user-estimate`)
+  const userElem = document.querySelector(`.${userId}.user-estimate`)
   userElem.innerHTML = points
 }
 
 const addUser = usersElem => ({userId, points}) => {
-  const userElem = document.createElement('li')
-  userElem.setAttribute('class', userId)
+  const userElem = document.createElement('dt')
   userElem.appendChild(document.createTextNode(userId))
+  userElem.setAttribute('class', 'col-8')
 
-  const estimateElem = document.createElement('span')
-  estimateElem.setAttribute('class', 'user-estimate')
-  userElem.appendChild(estimateElem)
+  const estimateElem = document.createElement('dd')
+  estimateElem.setAttribute('class', `${userId} user-estimate col-4`)
 
   usersElem.appendChild(userElem)
+  usersElem.appendChild(estimateElem)
 }
 
 const allHaveEstimated = users => {
