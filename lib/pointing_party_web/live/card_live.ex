658dead8a5111295760976db53d5cfae41bb5895
Michael Stalker <mtstalker@gmail.com>
2019-08-17 08:59:14 -0400

    Show next button

--- lib/pointing_party_web/live/card_live.ex
+++ lib/pointing_party_web/live/card_live.ex
@@ -13,6 +13,7 @@ defmodule PointingPartyWeb.CardLive do
     Endpoint.subscribe("users") # Does this need to be different than the Presence channel?
     {:ok, _} = Presence.track(self(), "users", username, %{points: nil})
     assigns = [
+      is_driving: false,
       party_has_started: false,
       show_votes: false,
       username: username,
@@ -25,7 +26,7 @@ defmodule PointingPartyWeb.CardLive do
   def handle_event("start_party", _, socket) do
     Endpoint.broadcast("users", "party_started", %{})
 
-    {:noreply, socket}
+    {:noreply, assign(socket, is_driving: true)}
   end
 
   def handle_event("vote", %{"points" => points}, socket) do
