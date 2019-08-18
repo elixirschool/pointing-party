7288f5a61565629c14d66e96fb1819ec310e6547
sophiedebenedetto <sophie.debenedetto@gmail.com>
2019-08-18 16:01:32 -0400

    update channel starter code

--- lib/pointing_party_web/channels/room_channel.ex
+++ lib/pointing_party_web/channels/room_channel.ex
@@ -15,26 +15,27 @@ defmodule PointingPartyWeb.RoomChannel do
     {:noreply, socket}
   end
 
-  def handle_in("user_estimated", %{"points" => points}, socket) do
-    # update votes for user presence
-    # calculate votes if everyone voted with the help of the VoteCalculator
-    # broadcast the 'winner'/'tie' event with a payload of %{points: points}
-
-    {:noreply, socket}
-  end
-
-  def handle_in("finalized_points", %{"points" => points}, socket) do
-    # update state by setting the current card to the next card
-    # broadcast the "new_card" message with a payload of %{card: new_current_card}
-
-    {:reply, :ok, socket}
-  end
-
   def handle_in("start_pointing", _params, socket) do
     updated_socket = initialize_state(socket)
     # broadcast the "new_card" message with a payload of %{card: current_card}
 
     {:reply, :ok, updated_socket}
+  end
+
+  def handle_in("user_estimated", %{"points" => points}, socket) do
+    # update votes for user presence
+    # if everyone voted, calculate story point estimate with the help of the VoteCalculator
+    # broadcast the 'winner'/'tie' event with a payload of %{points: points}
+
+    {:noreply, socket}
+  end
+
+  def handle_in("next_card", %{"points" => points}, socket) do
+    # update the current card's `points` value to `points` from the message payload
+    # update state by setting the current card to the next card
+    # broadcast the "new_card" message with a payload of %{card: new_current_card}
+
+    {:reply, :ok, socket}
   end
 
   defp initialize_state(%{assigns: %{cards: _cards}} = socket), do: socket
