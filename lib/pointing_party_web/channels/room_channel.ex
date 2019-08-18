e148d479b0b66460425631ee05cb409337689aa5
sophiedebenedetto <sophie.debenedetto@gmail.com>
2019-08-18 16:04:21 -0400

    provide channel helper function to save points and switch to next card

--- lib/pointing_party_web/channels/room_channel.ex
+++ lib/pointing_party_web/channels/room_channel.ex
@@ -31,8 +31,7 @@ defmodule PointingPartyWeb.RoomChannel do
   end
 
   def handle_in("next_card", %{"points" => points}, socket) do
-    # update the current card's `points` value to `points` from the message payload
-    # update state by setting the current card to the next card
+    save_vote_next_card(points, socket)
     # broadcast the "new_card" message with a payload of %{card: new_current_card}
 
     {:reply, :ok, socket}
@@ -46,5 +45,25 @@ defmodule PointingPartyWeb.RoomChannel do
     socket
     |> assign(:unvoted, cards)
     |> assign(:current, first)
+  end
+
+  defp save_vote_next_card(points, socket) do
+    # save the points on the card
+    latest_card =
+      socket.assigns
+      |> Map.get(:current)
+      |> Map.put(:points, points)
+
+    # fetch the next card from the list of cards
+    {next, remaining} =
+      socket.assigns
+      |> Map.get(:unvoted)
+      |> List.pop_at(0)
+
+    # update socket state by moving the current card into `voted` and the next card into `current_card`
+    socket
+    |> assign(:unvoted, remaining)
+    |> assign(:current, next)
+    |> assign(:voted, [latest_card | socket.assigns[:voted]])
   end
 end
