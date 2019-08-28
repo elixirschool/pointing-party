56ba1d3b0fde2e6676d679a1eeacfd11f64e505f
sophiedebenedetto <sophie.debenedetto@gmail.com>
2019-08-28 07:25:57 -0600

    update

--- lib/pointing_party_web/live/card_live.ex
+++ lib/pointing_party_web/live/card_live.ex
@@ -7,63 +7,40 @@ defmodule PointingPartyWeb.CardLive do
     # render the LiveView template here
   end
 
-  def mount(%{username: _username}, socket) do
-    {:ok, assign(socket, initial_state())}
+  def mount(%{username: username}, socket) do
+    {:ok, assign(socket, initial_state(username))}
   end
 
   ## Helper Methods ##
 
-  defp initial_state do
+  defp initial_state(username) do
     [
       current_card: nil,
       outcome: nil,
       is_pointing: false,
-      points_range: Card.points_range(),
       remaining_cards: [],
+      completed_cards: [],
       point_tally: nil,
       users: [],
-      completed_cards: []
+      username: username
     ]
   end
 
-  defp get_next_card(cards) do
-    List.pop_at(cards, 0)
-    # returns {next_card, remaining_cards}
-  end
+  defp save_vote_next_card(points, socket) do
+    latest_card =
+      socket.assigns
+      |> Map.get(:current_card)
+      |> Map.put(:points, points)
 
-  defp everyone_voted?(users) do
-    users
-    |> Enum.map(fn {_username, %{metas: [metas]}} -> Map.get(metas, :points) end)
-    |> Enum.all?(&(not is_nil(&1)))
-  end
-
-  defp calculate_story_points(users) do
-    VoteCalculator.calculate_votes(users)
-    # returns {outcome, point_tally}
-    # ex - {"winner", 3} or {"tie", [3, 5]}
-  end
-
-  defp update_state_with_story_point_calculation(outcome, point_tally, socket) do
-    socket
-    |> assign(:outcome, outcome)
-    |> assign(:point_tally, point_tally)
-  end
-
-  defp save_card_points(points, %{assigns: %{current_card: current_card}} = socket) do
-    updated_card = Map.put(current_card, :points, points)
+    {next_card, remaining_cards} =
+      socket.assigns
+      |> Map.get(:remaining_cards)
+      |> List.pop_at(0)
 
     socket
-    |> assign(:current_card, updated_card)
-  end
-
-  defp reset_state_for_next_card(%{assigns: %{cards: cards, current_card: current_card, completed_cards: completed_cards}} = socket) do
-    {next_card, remaining_cards} = get_next_card(cards)
-
-    socket
-    |> assign(:current_card, next_card)
     |> assign(:remaining_cards, remaining_cards)
-    |> assign(:completed_cards, [current_card | socket.assigns[:completed_cards]])
+    |> assign(:current_card, next_card)
     |> assign(:outcome, nil)
-    |> assign(:point_tally, nil)
+    |> assign(:completed_cards, [latest_card | socket.assigns[:completed_cards]])
   end
 end
